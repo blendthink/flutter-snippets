2e6c73fdc1f398a2b1747bef4db384c358083243
GitHub Action <action@github.com>
2022-03-24 22:57:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/blendthink/fluttersnippets/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.blendthink.fluttersnippets.services
+
+import com.github.blendthink.fluttersnippets.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
